7053062e5fa5232598ed3f2d2860b7a6af0b0b28
Eric <ericseals@ku.edu>
2019-08-07 16:30:29 -0500

    Trying to get image capture to work with BitMap

---
+++ app/src/main/java/com/example/mytcpclientapplication/DisplayImage.kt
@@ -0,0 +1,12 @@
+package com.example.mytcpclientapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayImage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_image)
+    }
+}
--- app/src/main/java/com/example/mytcpclientapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytcpclientapplication/MainActivity.kt
@@ -1,20 +1,117 @@
 package com.example.mytcpclientapplication
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val permissionCode = 1000
+    private val imageCaptureCode = 1001
+    private var imageUri: Uri? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        button.setOnClickListener{
-            val intent = Intent(this, TestConnectionSend::class.java)
-            startActivity(intent)
+        //button click
+        captureButton.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    == PackageManager.PERMISSION_DENIED) {
+                    //permission not accepted
+                    val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    //show popup to user to request
+                    requestPermissions(permission, permissionCode)
+                }
+                else {
+                    //permission already granted
+                    openCamera()
+                }
+            }
+            else{
+                //system version too old
+                openCamera()
+            }
         }
     }
+
+    private fun openCamera() {
+//        val values = ContentValues()
+//        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+//        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+//        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(cameraIntent, imageCaptureCode)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            permissionCode -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission granted from popup
+                    openCamera()
+                }
+                else {
+                    //permission denied
+                    Toast.makeText(this, "Permission Not Granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            imageCaptureCode -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    val myNewIntent = Intent()
+
+
+                    imageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Request not recognized", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        //called when image is taken
+//        if(resultCode == Activity.RESULT_OK && requestCode == imageCaptureCode){
+//            var bmp = data!!.extras!!.get("data") as Bitmap
+//            imageView.setImageBitmap(bmp)
+            //put the image on the image view
+//            imageView.setImageURI(imageUri)
+    }
 }
+
