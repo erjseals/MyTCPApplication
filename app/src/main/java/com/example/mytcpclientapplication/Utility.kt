0dd06bf943a8ff7392e759f8d2885dfda0a1ef55
Eric <ericseals@ku.edu>
2019-08-08 18:23:55 -0500

    Okay works for one-off with server !

--- app/src/main/java/com/example/mytcpclientapplication/Utility.kt
+++ app/src/main/java/com/example/mytcpclientapplication/Utility.kt
@@ -20,54 +20,12 @@ class Utility() : AsyncTask<ByteArray, Void, Void>() {
             val dataOutputStream = DataOutputStream(socket.getOutputStream())
             val dataInputStream = DataInputStream(socket.getInputStream())
 
-//            dos.writeInt(p0[0]!!.size)
-//            dos.write(p0[0], 0, p0[0]!!.size)
+            dataOutputStream.writeInt(p0[0]!!.size)
+            dataOutputStream.write(p0[0], 0, p0[0]!!.size)
 
 
-            val arr = byteArrayOf(
-                0x00.toByte(),
-                0x01.toByte(),
-                0x00.toByte(),
-                0x10.toByte(),
-                0x00.toByte(),
-                0x01.toByte(),
-                0x00.toByte(),
-                0x1F.toByte(),
-                0x60.toByte(),
-                0x1D.toByte(),
-                0xA1.toByte(),
-                0x09.toByte(),
-                0x06.toByte(),
-                0x07.toByte(),
-                0x60.toByte(),
-                0x85.toByte(),
-                0x74.toByte(),
-                0x05.toByte(),
-                0x08.toByte(),
-                0x01.toByte(),
-                0x01.toByte(),
-                0xBE.toByte(),
-                0x10.toByte(),
-                0x04.toByte(),
-                0x0E.toByte(),
-                0x01.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x06.toByte(),
-                0x5F.toByte(),
-                0x1F.toByte(),
-                0x04.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x18.toByte(),
-                0x1D.toByte(),
-                0xFF.toByte(),
-                0xFF.toByte()
-            )
-            dataOutputStream.write(arr)
-            val req = Arrays.toString(arr)
-            Log.i("TRACING_CODE", "$req")
+//            val req = Arrays.toString(arr)
+//            Log.i("TRACING_CODE", "$req")
 
             dataInputStream.close()
             dataOutputStream.close()
