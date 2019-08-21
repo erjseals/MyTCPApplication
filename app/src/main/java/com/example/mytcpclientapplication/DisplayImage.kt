f0cbd22503508f24781fd64b82d07f24cec9f217
Eric <ericseals@ku.edu>
2019-08-21 17:01:32 -0500

    The gallery still doesn't send, but have cute fab menu <3

--- app/src/main/java/com/example/mytcpclientapplication/DisplayImage.kt
+++ app/src/main/java/com/example/mytcpclientapplication/DisplayImage.kt
@@ -9,7 +9,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_image.*
--- app/src/main/java/com/example/mytcpclientapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytcpclientapplication/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
