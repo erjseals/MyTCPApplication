5769bcd6c470d15ed5616e0d657b96b95f6908aa
Eric <ericseals@ku.edu>
2019-08-23 19:08:47 -0500

    some full screen stuff

--- app/src/main/java/com/example/mytcpclientapplication/DisplayImage.kt
+++ app/src/main/java/com/example/mytcpclientapplication/DisplayImage.kt
@@ -4,11 +4,13 @@ import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_image.*
@@ -107,6 +109,36 @@ class DisplayImage : AppCompatActivity() {
         val myUtilityReceive = UtilityReceive(progressBar)
         myUtilityReceive.execute(imageView2)
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (
+                // View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
 
     private fun startImageCapture(){
 
--- app/src/main/java/com/example/mytcpclientapplication/UtilitySend.kt
+++ app/src/main/java/com/example/mytcpclientapplication/UtilitySend.kt
@@ -15,7 +15,7 @@ class UtilitySend : AsyncTask<ByteArray, Void, Int>() {
         )
 
         try {
-            val socket = Socket("10.1.10.181", 8080)
+            val socket = Socket("192.168.43.89", 8080)
 
             if (socket.isConnected) {
                 Log.i(
